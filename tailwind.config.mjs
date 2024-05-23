d2a47c5feaaf5a2dcbbdad7db2c25e4498adc2f5
Verónica Andrea García <veroagarcia90@gmail.com>
2024-05-23 09:48:43 -0300

    header y footer

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				background: '#d7e7e0',
+				primary: '#e55c78', // Ejemplo de color rosa
+				primaryLight: '#ff6e90',
+				primaryDark:'#b74a60',
+				secondary: '#4db081', // Ejemplo de color verde
+				secondaryLight: '#5cdb9b',
+				secondaryDark:'#3e8d67',
+				contrast:'#2f4858',
+				gray:'#d2d2d2'
+			},
+		},
+	},
+	plugins: [],
+}
